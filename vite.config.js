099740eeae155e1123681ec976c431d9fecb8b7c
danil-shc <denzel_R@mail.ru>
2026-05-25 21:14:27 +0300

    fix: add correct slashes to vite base path

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     tailwindcss(),
   ],
-  base: 'Pchel_Grind_Project',
+  base: '/Pchel_Grind_Project/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
